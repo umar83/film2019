43f7e26f7e558fa21d40a9b22bbf702ccf60a6ee
umar83 <umarhatob@gmail.com>
2019-07-04 16:03:07 +0700

    Update hellboy2019.php

--- hellboy2019.php
+++ hellboy2019.php
@@ -7,44 +7,16 @@
 	<body>
 				<div id="myElement">Loading the player...</div>
 <script type="text/javascript">
-var playerInstance = jwplayer("myElement")<head>
-	    <script type="text/javascript" src="https://cdn.rawgit.com/umar83/jwplayer/f13c05d7/jwplayer-7.9.3/jwplayermetro.js"></script>
-<script>jwplayer.key="ypdL3Acgwp4Uh2/LDE9dYh3W/EPwDMuA2yid4ytssfI=";</script>
-		<title>Test Drive Grabber</title>
-	</head>
-
-	<body>
-				<div id="myElement">Loading the player...</div>
-<script type="text/javascript">
 var playerInstance = jwplayer("myElement")
 playerInstance.setup({
     playlist: [{
         file: "https://www.googleapis.com/drive/v3/files/14Yth_LfSDaDAUmUFTjZpMFejVKSFLTkP?alt=media&key=AIzaSyBEyekSgpUqNQNV4VNYJ9UEZ3tPZMrIHgY",
         type: "video/mp4",
         image: "https://drive.google.com/thumbnail?id=14Yth_LfSDaDAUmUFTjZpMFejVKSFLTkP&authuser=0&sz=w640-h360-n-k-rw",
-        tracks: [{ 
+        aspectratio: "16:9",
+	    tracks: [{ 
             file: "https://indoxxi.show/8806bf91-c804-49b8-9397-4986ebaa8229", 
             label: "indonesia",
-            kind: "captions",
-            "default": true 
-        }]
- }],
-    captions: {
-        color: '#fc6',
-        fontSize: 24,
-        backgroundOpacity: 05
-    }   
-  
-});
-</script>
-playerInstance.setup({
-    playlist: [{
-        file: "https://mopis.000webhostapp.com/gdrive/embed.php",
-        type: "video/mp4",
-        image: "https://fanart.tv/fanart/movies/9533/moviethumb/red-dragon-522d41d16142d.jpg",
-        tracks: [{ 
-            file: "https://content.jwplatform.com/tracks/GE2bznMJ.srt", 
-            label: "English",
             kind: "captions",
             "default": true 
         }]
